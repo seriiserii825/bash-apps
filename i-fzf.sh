2d4a197b1373608dd8dfe1553cc13cf82b355f35
serii <seriiburduja@gmail.com>
2023-12-11 11:26:17 +0200

    feat: i-fzf

--- i-fzf.sh
+++ i-fzf.sh
@@ -1,9 +1,17 @@
-#! /bin/bash
-
+#! /bin/bash -x
 installApp() {
-  cd ~
-  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-  ~/.fzf/install
-  sudo apt intall -y ripgrep universal-ctags silversearcher-ag fd-find
+  has_fzf=$(which fzf)
+  if [ -z "$has_fzf" ]; then
+    cd ~
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+    sudo apt intall -y ripgrep universal-ctags silversearcher-ag fd-find
+  fi
+  has_bat=$(which bat)
+  if [ -z "$has_bat" ]; then
+    cd ~/Downloads
+    wget -O bat.deb  https://github.com/sharkdp/bat/releases/download/v0.24.0/bat-musl_0.24.0_amd64.deb
+    sudo dpkg -i bat.deb
+  fi
 }
 installApp
--- i-zsh.sh
+++ i-zsh.sh
@@ -1,26 +1,21 @@
 #! /bin/bash
-
 installApp() {
   cd ~
   sudo apt install -y zsh
   curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
   sudo  chsh -s $(which zsh)
   sudo chsh -s /bin/zsh serii
-
   if [ ! -d ~/zsh-autocomplete ]; then
     git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
     source ~/zsh-autocomplete/zsh-autocomplete.plugin.zsh
   fi
-
   if [ ! -d ~/zsh-syntax-highlighting ]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
     source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
   fi
-
   if [ ! -d ~/.zsh/zsh-autosuggestions ]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
     source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
   fi
-
 }
 installApp
