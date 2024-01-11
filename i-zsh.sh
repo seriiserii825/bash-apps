fe522cddbafc823f8d9e6010125a4b44eaf4283c
serii <seriiburduja@gmail.com>
2024-01-11 11:43:11 +0200

    feat: sxiv

--- i-zsh.sh
+++ i-zsh.sh
@@ -18,5 +18,6 @@ installApp() {
     source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
   fi
   wget "https://raw.githubusercontent.com/rupa/z/master/z.sh" -O "~/z.sh"
+  sudo apt install sxiv vifm
 }
 installApp
