7a08ec5635d8f6d79af29bbfe9c3a039082b5cdf
serii <seriiburduja@gmail.com>
2024-06-19 10:59:22 +0300

    upd: alacrity

--- i-alacritty.sh
+++ i-alacritty.sh
@@ -4,5 +4,7 @@ installApp() {
   Install cargo
   Install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
   cargo install alacritty
+  mkdir ~/.config/alacritty
+  ln -s ~/xubuntu/alacritty.toml ~/.config/alacritty/alacritty.toml
 }
 installApp
